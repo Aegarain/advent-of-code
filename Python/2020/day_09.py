59bc5aae6f52e29dc1ab6b743696c31a91127f0c
Aegarain <zmailer2005@gmail.com>
2020-12-09 13:54:42 -0800

    Day 9

---
+++ Python/2020/day_09.py
@@ -0,0 +1,35 @@
+import re
+input_file = open("day_09_example.txt")
+input_text = input_file.read()
+
+list = list(int(x) for x in input_text.split("\n"))
+
+preamble = 5
+
+def check_valid(number, range):
+    Valid = False
+    for x in range:
+        if Valid:
+            break
+        for y in range:
+            if y == x:
+                break
+            if y + x == number:
+                Valid = True
+                break
+    return Valid
+
+
+def XMAS(input, length):
+    total_length = len(input) - length
+    pos = int(length)
+    while True:
+        lower_bound = pos - length
+        if not check_valid(input[pos],input[lower_bound:pos]):
+            return input[pos]
+            break
+        else:
+            pos +=1
+
+invalid = XMAS(list, preamble)
+print(invalid)
